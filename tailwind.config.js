b8b89799363c449f50287df956a38d3d706a8b2c
BlackDragon197 <91381539+BlackDragon197@users.noreply.github.com>
2022-07-09 12:50:56 +0300

    1st

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+  ],
+  theme: {
+    fontFamily:{
+      "futura": "Futura PT",
+      "montserrat": "'Montserrat', sans-serif",
+    },
+    maxWidth:{
+      '1410': '1410px',
+      '380': '380px',
+      '740': '740px',
+      'fit': 'fit-content'
+    },
+    minWidth:{
+      '380': '380px',
+      '740': '740px'
+    },
+    borderRadius:{
+      'rounded-3': '3px',
+    },
+    boxShadow:{
+      "1": 'box-shadow: 0px 3.6213px 24.4438px rgba(0, 0, 0, 0.25);'
+    },
+    extend: {
+      
+    },
+    screens: {
+      'xs': '375px',
+      'sm': '768px',
+      'md': '1024px',
+      'lg': '1280px',
+      'xl': '1440px',
+    },
+
+  },
+  plugins: [],
+}
