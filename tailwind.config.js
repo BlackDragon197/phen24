6100b97669def5a40ca9a9da5059e8c98e8d9456
BlackDragon197 <91381539+BlackDragon197@users.noreply.github.com>
2022-08-06 23:28:24 +0300

    9block

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,7 @@ module.exports = {
         'all-day': 'linear-gradient(180deg, #FF9900 0%, #EA552E 100%);',
         'all-night': 'linear-gradient(180deg, #26AAE0 0%, #693D90 64.06%);',
         'formula-day': 'linear-gradient(180deg, #EA552E 19.48%, #FF9900 78.45%);',
+        'table-rect': 'linear-gradient(180deg, #FF9900 0%, #FD1111 100%);'
       },
       spacing:{
         'reviews': '75px'
